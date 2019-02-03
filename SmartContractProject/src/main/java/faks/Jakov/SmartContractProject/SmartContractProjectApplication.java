a51bdfcde966816e9126b75e9d065db73f67a546
NetkoNefarious <jmagazin5@gmail.com>
2019-02-03 23:12:45 +0100

    Pregled Ethereum računa sada radi

--- SmartContractProject/src/main/java/faks/Jakov/SmartContractProject/SmartContractProjectApplication.java
+++ SmartContractProject/src/main/java/faks/Jakov/SmartContractProject/SmartContractProjectApplication.java
@@ -1,14 +1,19 @@
 package faks.Jakov.SmartContractProject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SmartContractProjectApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContractProjectApplication.class, args);
 	}
-
+	
+	@Bean
+	public Logger getLogger() {
+		return LoggerFactory.getLogger(SmartContractProjectApplication.class);
+	}
 }
-
