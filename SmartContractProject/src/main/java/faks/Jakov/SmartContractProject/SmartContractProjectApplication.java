ac3e231d82be1de68cc736d0866540fe26b88b8e
NetkoNefarious <jmagazin5@gmail.com>
2019-02-03 17:50:09 +0100

    Dodan Spring Boot projekt

---
+++ SmartContractProject/src/main/java/faks/Jakov/SmartContractProject/SmartContractProjectApplication.java
@@ -0,0 +1,14 @@
+package faks.Jakov.SmartContractProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContractProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContractProjectApplication.class, args);
+	}
+
+}
+
